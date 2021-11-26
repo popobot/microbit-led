fcbd04f77ef1e7aab703070d8b35b2b82cd6aa47
Masanori Ichikawa <ichimasan@gmail.com>
2021-11-26 18:29:57 +0900

    ロゴをタップしたときのカラフルモードを追加

--- main.ts
+++ main.ts
@@ -1,26 +1,34 @@
-const p0 = neopixel.create(DigitalPin.P0, 25, NeoPixelMode.RGB)
-const p8 = neopixel.create(DigitalPin.P8, 25, NeoPixelMode.RGB)
-
-const hue = 0
-const saturation = 0
-let luminosity = 50
-
-function light() {
-    p0.showColor(neopixel.hsl(hue, saturation, luminosity))
-    p8.showColor(neopixel.hsl(hue, saturation, luminosity))
-}
-
-light()
-
-input.onButtonPressed(Button.AB, function (){
-    luminosity = 50
-    light()
-})
 input.onButtonPressed(Button.A, function () {
     luminosity = Math.min(luminosity + 10, 100)
+    light()
+})
+input.onButtonPressed(Button.AB, function () {
+    luminosity = 50
+    hue = 0
+    saturation = 0
     light()
 })
 input.onButtonPressed(Button.B, function () {
     luminosity = Math.max(luminosity - 10, 0)
     light()
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    hue = randint(0, 360)
+    saturation = 100
+    light()
+})
+function light () {
+    p0.showColor(neopixel.hsl(hue, saturation, luminosity))
+    p8.showColor(neopixel.hsl(hue, saturation, luminosity))
+}
+let saturation = 0
+let hue = 0
+let luminosity = 0
+let p8: neopixel.Strip = null
+let p0: neopixel.Strip = null
+p0 = neopixel.create(DigitalPin.P0, 25, NeoPixelMode.RGB)
+p8 = neopixel.create(DigitalPin.P8, 25, NeoPixelMode.RGB)
+luminosity = 50
+hue = 0
+saturation = 0
+light()
